f12d1e81dbbceaf15b801b6798c6450f82ad4f91
Javonet <support@javonet.com>
2019-08-14 23:57:37 +0000

    init

---
+++ src/holamundo/clasemensaje.java
@@ -0,0 +1,62 @@
+package holamundo;
+
+import Common.Activation;
+import static Common.JavonetHelper.Convert;
+import static Common.JavonetHelper.getGetObjectName;
+import static Common.JavonetHelper.getReturnObjectName;
+import static Common.JavonetHelper.ConvertToConcreteInterfaceImplementation;
+import Common.JavonetHelper;
+import Common.MethodTypeAnnotation;
+import com.javonet.Javonet;
+import com.javonet.JavonetException;
+import com.javonet.JavonetFramework;
+import com.javonet.api.NObject;
+import com.javonet.api.NEnum;
+import com.javonet.api.keywords.NRef;
+import com.javonet.api.keywords.NOut;
+import com.javonet.api.NControlContainer;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Iterator;
+import java.lang.*;
+import holamundo.*;
+
+public class clasemensaje {
+  protected NObject javonetHandle;
+
+  public clasemensaje() {
+    try {
+      javonetHandle = Javonet.New("holamundo.clasemensaje");
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+    }
+  }
+
+  public clasemensaje(NObject handle) {
+    this.javonetHandle = handle;
+  }
+
+  public void setJavonetHandle(NObject handle) {
+    this.javonetHandle = handle;
+  }
+  /** Method */
+
+  @MethodTypeAnnotation(type = "Method")
+  public java.lang.String mensaje() {
+    try {
+      java.lang.String res = javonetHandle.invoke("mensaje");
+      if (res == null) return "";
+      return (java.lang.String) res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return "";
+    }
+  }
+
+  static {
+    try {
+      Activation.initializeJavonet();
+    } catch (java.lang.Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
